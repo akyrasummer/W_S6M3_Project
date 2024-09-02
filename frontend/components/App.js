3d580726889638d0ff235cf3decd13403eb48d85
Akyra Summer <su.akyra00@gmail.com>
2024-09-02 15:59:45 -0700

    Wip

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const api_key = 'DEMO_KEY'
+const URL = `https://api.nasa.gov/planetary/apod?api_key=${api_key}`
 
 function App() {
-  return (
+  const [apod, setApod] = useState()
+useEffect(() => {
+  function fetchPhoto() {
+    axios.get(URL)
+    .then(res => {
+      console.log(res.data)
+      setApod(res.data)
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+  }
+  fetchPhoto()
+   setApod()
+}, [])
+return (
     <p>
       Read through the instructions in the README.md file to build your NASA
       app! Have fun <span role="img" aria-label='go!'>🚀</span>!
